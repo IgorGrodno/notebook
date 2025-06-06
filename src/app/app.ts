69989fae0ec926f731d90393d1b20bfbdf4ca0a1
IgorGrodno <noobprofesor@gmail.com>
2025-06-06 17:55:17 +0300

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Content } from "./content/content";
+import { Footer } from "./footer/footer";
+
+@Component({
+  selector: 'app-root',
+  imports: [Header, Content, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'notebook';
+}
