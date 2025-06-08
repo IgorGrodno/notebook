26b2e232826ccd0a641924a23eb6e0d5c5ad8116
IgorGrodno <noobprofesor@gmail.com>
2025-06-08 21:20:40 +0300

    base html added

---
+++ src/app/services/noteService.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { note } from '../interfaces/note.interface';
+import { searchCriteries } from '../interfaces/searchCriteries.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NoteService {
+  constructor(private http: HttpClient) {}
+
+  private noteUrl = 'fakeDB/notes.json';
+
+  getNotesPage(
+    pageNumber: number,
+    pageSize: number,
+    searchCriteries?: searchCriteries
+  ) {
+    let url = `${this.noteUrl}?page=${pageNumber}&size=${pageSize}`;
+
+    if (searchCriteries) {
+      if (searchCriteries.searchText) {
+        url += `&title=${encodeURIComponent(searchCriteries.searchText)}`;
+      }
+      if (searchCriteries.startDate) {
+        url += `&startDate=${searchCriteries.startDate.toISOString()}`;
+      }
+      if (searchCriteries.endDate) {
+        url += `&endDate=${searchCriteries.endDate.toISOString()}`;
+      }
+    }
+
+    return this.http.get<note[]>(url);
+  }
+
+  addNote(note: note) {
+    return this.http.post<note>(this.noteUrl, note);
+  }
+}
